7ac07c5191d24e12d4b32be9897c008372d5f8b1
angine chakma <angineckz01@gmail.com>
2026-04-13 11:23:50 +0600

    implement search functionality and some functions

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -7,11 +7,14 @@ const Apps = () => {
     const appsData = useLoaderData()
     const [search, setSearch] = useState("")
     const handleSearch = (e) => {
-        // e.preventDefault()
+        e.preventDefault()
         const searchResult = e.target.value;
         setSearch(searchResult);
-        console.log(searchResult);
     }
+
+    const filteredApps = appsData.filter(app =>
+        app.title.toLowerCase().includes(search.toLowerCase())
+    );
     return (
         <div className='my-12 space-y-4'>
             <div className='text-center my-8'>
@@ -19,7 +22,7 @@ const Apps = () => {
                 <p>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
             <div className='md:flex space-y-3 md:space-y-0 items-center justify-between'>
-                <h3 className='font-semibold text-2xl'> ({appsData.length}) Apps Found</h3>
+                <h3 className='font-semibold text-2xl'> ({filteredApps.length}) Apps Found</h3>
                 <form className='border flex gap-3 items-center border-gray-300 p-2 rounded-md'>
                     <FaSearch/>
                     <input onChange={handleSearch} defaultValue={search} type="text" placeholder='searchBar' />
@@ -27,9 +30,9 @@ const Apps = () => {
             </div>
             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-3'>
                 {
-                    appsData.map(app => <TrendingApp
+                    filteredApps.map(app => <TrendingApp
                         key={app.id}
-                        app={app}></TrendingApp>)
+                        app={app} />)
                 }
             </div>
         </div>
